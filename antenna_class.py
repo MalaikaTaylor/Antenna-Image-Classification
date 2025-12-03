1527c7d96d49bc4b254ba07a961ae23ad9879e36
MalaikaTaylor <mtaylor389@gatech.edu>
2025-12-03 15:28:47 -0500

    Create antenna_class.py

---
+++ antenna_class.py
@@ -0,0 +1,10 @@
+#import necessary packages
+import os
+import numpy as np
+import keras
+import matplotlib.pyplot as plt
+from keras import layers
+from tensorflow import data as tf_data
+import tensorflow as tfp
+
+# load and visualize the data
